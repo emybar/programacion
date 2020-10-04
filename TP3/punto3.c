0a8780fb5ecea7c1df6b179659048977387be90f
Guadalupe Delorme <guadalupedelorme@gmail.com>
2020-10-04 18:07:36 -0300

    Respuesta a consulta punto 3

--- TP3/punto3.c
+++ TP3/punto3.c
@@ -3,34 +3,50 @@
 #include <ctype.h>
 #include <stdbool.h>
 
-bool es_vocal (char carac);
+int es_vocal(char carac);
+// Todo lo que hacés acá en main debería ser tu función, incluída la función es_vocal
+// Fijate que el ejercicio te pide que el main sea como te pongo a continuación.
+// Si seguís el otro programa instrucción por instrucción te vas a dar cuenta lo que te comenté.
+// Fijate que en el grupo de consulta yo subí 2 archivos para que configuren el VSCode para que lo puedan ejecutar así, viendo el valor de las variables paso a paso
+// Si no podés configurarlo avisame
 
-int main(){
-    char carac;
+// int main()
+// {
+// 	transformar();
+// 	return 0;
+// }
+
+int main()
+{
+	char carac;			
 	int nroletra = 0;
-    printf("Ingrese una oracion \n");
-	scanf("%c",&carac);
-	while(carac !='.') {
-        if(nroletra == 0 && es_vocal(carac) ) {
-    
+	printf("Ingrese una oracion \n");
+	scanf("%c", &carac);
+	while (carac != '.')
+	{
+		if (nroletra == 0 && es_vocal(carac))
+		{
+
 			carac = toupper(carac);
 		}
 		nroletra++;
-		if(carac == ' ')
+		if (carac == ' ')
 			nroletra = 0;
-		printf("%c",carac);
-		scanf("%c",&carac);
+		printf("%c", carac);
+		scanf("%c", &carac);
 	}
-	printf("."); 
+	printf(".");
+	return 0;
 }
 
-
-
-bool es_vocal(char carac){
-        if (carac=='a'|| carac=='e'|| carac=='i'|| carac=='o'||carac=='u'){
-            return 1; 
-        }else{
-            return 0 ;
-        }
-} 
-
+int es_vocal(char carac)
+{
+	if (carac == 'a' || carac == 'e' || carac == 'i' || carac == 'o' || carac == 'u')
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+}
